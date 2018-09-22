bad7571c06b35fa7841a52e48b7488def17fc9bf
Andres R <zero389@gmail.com>
2018-09-22 12:44:51 -0500

    Heroku config

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+/* Database config */
+
+$host = 'w29ifufy55ljjmzq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username = 'rdkhvluvithx1sh8';
+$password = 'qwlgy6eube3kifc7';
+$database = 'i67g5hvxb8ho8ijt';
+
 return [
 
     /*
@@ -41,11 +48,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

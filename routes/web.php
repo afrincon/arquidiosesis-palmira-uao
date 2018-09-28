a687de8fc20c80ffa1458e7c137180be8204006e
Andres R <zero389@gmail.com>
2018-09-28 14:51:43 -0500

    changes on routes

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('iglesias', 'IglesiaController')->only([
+Route::resource('/iglesias', 'IglesiaController')->only([
     'index', 'create', 'store'
 ]);

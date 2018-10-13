f563ac7e83cdbf7008c2f73da95b834740305261
Lab Informatica <vanessa.velasquez_or@uao.edu.co>
2018-10-13 14:46:14 -0500

    Correccion de ayudas

---
+++ app/Http/Controllers/TipoAyudaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TipoAyuda;
+use Illuminate\Http\Request;
+
+class TipoAyudaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TipoAyuda  $tipoAyuda
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TipoAyuda $tipoAyuda)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TipoAyuda  $tipoAyuda
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TipoAyuda $tipoAyuda)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TipoAyuda  $tipoAyuda
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TipoAyuda $tipoAyuda)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TipoAyuda  $tipoAyuda
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TipoAyuda $tipoAyuda)
+    {
+        //
+    }
+}

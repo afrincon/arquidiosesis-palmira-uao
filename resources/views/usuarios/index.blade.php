6d56f2d44bb6bea41193cc80fe58ee381ccb99f2
FELLSAT <willysaty1393@hotmail.com>
2018-10-13 10:52:48 -0500

    consulta iglesia Andres R

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -3,12 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <h1>Usuarios</h1>
-
-        <br/>
-
-        <div class="col-4">
-            <a class="btn btn-primary" href="{{ url('/usuarios/crear') }}">Crear iglesia</a>
+        <div class="col-10" align="center">
+			<h1>Usuarios</h1>
+		</div>
+		
+		<div class="col-2" align="left">
+			<span class="glyphicon glyphicon-plus" aria-hidden="true">fg</span>
+			
+			<a class="btn btn-primary" href="{{ url('/usuarios/crear') }}">Crear usuario</a>
         </div>
 
         @include('usuarios.partials.table')
--- resources/views/usuarios/partials/table.blade.php
+++ resources/views/usuarios/partials/table.blade.php
@@ -4,7 +4,6 @@
       <th scope="col">#</th>
       <th scope="col">Nombre</th>
       <th scope="col">Correo</th>
-      <th scope="col">Contraseña</th>
       <th scope="col">Estado</th>
       <th scope="col">Acciones</th>
     </tr>
@@ -15,7 +14,6 @@
         <td> {{ $usuario->id }}</td>
         <td> {{ $usuario->name }}</td>
         <td> {{ $usuario->email }}</td>
-        <td> {{ $usuario->password }}</td>
         <td> {{ $usuario->estado }}</td>
         <td>
           <a class="btn btn-primary" href="{{ url("usuarios/{$usuario->id}/editar") }}">Editar</a>

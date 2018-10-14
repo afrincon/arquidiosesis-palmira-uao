b7aaee3e2aefcb8302da567296657f8a3682f044
mauro550 <ramit.medrano@uao.edu.co>
2018-10-14 13:50:56 -0500

    Avance2-BeneficiariosEditar

--- app/Http/Controllers/BeneficiarioController.php
+++ app/Http/Controllers/BeneficiarioController.php
@@ -18,7 +18,7 @@ class BeneficiarioController extends Controller
         return view('beneficiarios.index', compact('beneficiarios'));
     }
     public function create() {
-        $users = User::all('id','name');
+        $users = User::all('id_beneficiario','nombre');
         #dd($users);
         $tipoDocumento = tipo_documento::all();
         return view('beneficiarios.create',compact('users','tipoDocumento'));
@@ -38,6 +38,45 @@ class BeneficiarioController extends Controller
         #dd($data);
         $beneficiario  = new beneficiario($data);
         $beneficiario->save();
+        return redirect()->route('beneficiarios.index')->with('success', 'Beneficiario agregado correctamente');
+    }
+
+    public function edit($id) {
+        $beneficiario = Beneficiario::findOrFail($id);
+        #dd($iglesia);
+        $users = User::all('id_beneficiario','nombre');
+        return view('beneficiarios.edit', compact('beneficiario','users'));
+    }
+
+    public function update($id)
+    {
+        $data = request()->validate([
+            'documento' => 'required',
+            'id_tipo_documento' => 'required',
+            'nombre' => 'required',
+            'apellido' => 'required',
+            'estado' => 'required',
+            'direccion' => 'required',
+            'telefono' => 'required|max:10',
+            'clasificacion' => 'required',
+        ]);
+
+        $beneficiario = Beneficiario::findOrFail($id);
+
+        $beneficiario->documento = $data['documento'];
+        $beneficiario->id_tipo_documento = $data['id_tipo_documento'];
+        $beneficiario->nombre = $data['nombre'];
+        $beneficiario->apellido = $data['apellido'];
+        $beneficiario->estado = $data['estado'];
+        $beneficiario->direccion = $data['direccion'];
+        $beneficiario->telefono = $data['telefono'];
+        $beneficiario->clasificacion = $data['clasificacion'];
+        $beneficiario->save();
+
         return redirect()->route('beneficiarios.index');
+    }
+
+    public function show($id){
+        return view('beneficiarios.show', ['beneficiario' => beneficiario::findOrFail($id)]);
     }
 }
--- resources/views/beneficiarios/partials/fields.blade.php
+++ resources/views/beneficiarios/partials/fields.blade.php
@@ -1,3 +1,39 @@
 <form method="post" action="{{ route('beneficiarios.update', $beneficiario->id) }}" >
 @method('put')
 {{ csrf_field() }}
+    <div class="col-sm-12 col-md-12">
+        <div class="form-group row">
+            <label for="inputBeneficiario" class="col-sm-3 col-form-label">Nombre</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" name="nombre" id="inputBeneficiario" autofocus value="{{ old('nombre') }}">
+            </div>
+
+            @if($errors->has('nombre'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('nombre') }}</strong>
+                </span>
+            @endif
+        </div>
+
+        <div class="form-group row">
+            <label for="inputBeneficiario" class="col-sm-3 col-form-label">Apellido</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" name="apellido" id="inputBeneficiario" autofocus value="{{ old('apellido') }}">
+            </div>
+
+            @if($errors->has('apellido'))
+                <span class="help-block">
+          <strong>{{ $errors->first('apellido') }}</strong>
+        </span>
+            @endif
+        </div>
+
+
+
+
+        <div class="form-group col-sm-12 col-md-8">
+            <button type="submit" class="btn btn-primary">Guardar</button>
+            <a class="btn btn-primary" href="{{ url('/beneficiarios') }}" role="button">Salir</a>
+        </div>
+    </div>
+</form>

9a8b80249473bc135cd3fc3999c06570a6e201a5
juandier <juandier_96@hotmail.com>
2018-10-14 20:23:51 -0500

    correccion show1

---
+++ resources/views/beneficiarios/show.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="col-sm-12 col-md-12" align="center">
+	<h1>Consulta beneficiario</h1>
+</div>
+<br>
+<div class="col-sm-12 col-md-12">
+  <div class="form-group row">
+      <label for="inputBeneficiario" class="col-sm-3 col-form-label">Nombre beneficiario</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="name" value="{{ $beneficiario->name  }}" disabled>
+      </div>
+    </div>
+    <div class="col-sm-12 col-md-12">
+  <div class="form-group row">
+      <label for="inputBeneficiario" class="col-sm-3 col-form-label">Apellido beneficiario</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="apellido" value="{{ $beneficiario->apellido  }}" disabled>
+      </div>
+    </div>
+
+<div class="col-sm-12 col-md-12">
+  <div class="form-group row">
+      <label for="inputTipodocumento" class="col-sm-3 col-form-label">tipo documento</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="id_tipo_documento" value="{{ $usuario->id_tipo_documento  }}" disabled>
+      </div>
+    </div>
+
+<div class="col-sm-12 col-md-12">
+  <div class="form-group row">
+      <label for="inputDocumento" class="col-sm-3 col-form-label">Documento</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="documento" value="{{ $usuario->documento  }}" disabled>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="inputTelefono" class="col-sm-3 col-form-label">Telefono</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="telefono" value="{{ $beneficiario->telefono }}" disabled>
+      </div>
+      
+    </div>
+    
+<div class="col-sm-12 col-md-12">
+  <div class="form-group row">
+      <label for="inputDireccion" class="col-sm-3 col-form-label">Direccion</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="direccion" value="{{ $usuario->direccion  }}" disabled>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="inputEstado" class="col-sm-3 col-form-label">Estado</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="estado" value="{{ $beneficiario->estado }}" disabled>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="inputClasificacion" class="col-sm-3 col-form-label">Clasificacion</label>
+      <div class="col-sm-6">
+        <input type="text" class="form-control" name="clasificacion" value="{{ $beneficiario->clasificacion }}" disabled>
+      </div>
+    </div>
+
+	<div class="col-sm-12 col-md-12" align="center">
+		<a class="btn btn-primary" href="{{ url('/beneficiarios') }}" role="button">Salir</a>
+	</div>	
+   </div>
+</div>
+@endsection

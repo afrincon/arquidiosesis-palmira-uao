8849474baf83354930f87c5897fd3acb808d7274
Andres R <zero389@gmail.com>
2018-10-15 11:16:43 -0500

    inicio + seguridad

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,21 +5,23 @@
     <div class="row principal">
         <h1>Opciones usuario</h1>
         <br/>
-        <div class="col-sm-6">
+        <div class="col-sm-12">
         <a href="{{ url('/beneficiarios')}}" class="btn btn-primary btn-lg">Beneficiarios</a>
         </div>
-        <div class="col-sm-6">
+        <br>
+        <div class="col-sm-12">
         <a href="{{ url('/ayudas')}}" class="btn btn-primary btn-lg">Ayudas</a>
         </div>
     </div>
     <hr>
-    <div class="row">
+    <div class="row principal">
         <h1>Opciones administrador</h1>
         <br/>
-        <div class="col-sm-6">
+        <div class="col-sm-12">
             <a href="{{ url('/usuarios')}}" class="btn btn-primary btn-lg">Usuarios</a>
         </div>
-        <div class="col-sm-6">
+        <br>
+        <div class="col-sm-12">
             <a href="{{ url('/iglesias')}}" class="btn btn-primary btn-lg">Iglesias</a>
         </div>
     </div>   

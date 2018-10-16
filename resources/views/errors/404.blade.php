184e6b31fc6bdb93a7a26f1dac6765fde7ab54fc
Andres R <zero389@gmail.com>
2018-10-15 20:31:27 -0500

    404 + show iglesias

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row principal">
+        <div class="col-sm-12">
+            <h1>Pagina se encuentra disponible</h1>
+        </div>
+        <div class="col-sm-12">
+            <a href="{{ url('/inicio') }}" class="btn btn-primary">Regresar</a>
+        </div>
+    </div>
+</div>
+@endsection

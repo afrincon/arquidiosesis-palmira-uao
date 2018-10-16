1fbbb2277c1dad12fffd4a8ce9ce697a7911154f
vhenryalejandro <43220433+vhenryalejandro@users.noreply.github.com>
2018-10-15 20:37:52 -0500

    ExtrasAyudas

--- resources/views/ayudas/partials/tablaAyuda.blade.php
+++ resources/views/ayudas/partials/tablaAyuda.blade.php
@@ -17,7 +17,7 @@
           <td> {{ $ayuda->id_ayuda }}</td>
           <td> {{ $ayuda->fecha_ayuda }}</td>
           <td> {{ $ayuda->tipoAyuda->descripcion }}</td>
-          <td> {{ $ayuda->beneficiario->documento }}</td>
+          <td><a href="{{ url("beneficiarios/{$ayuda->beneficiario->id_beneficiario}") }}"> {{ $ayuda->beneficiario->documento }} </a></td>
           <td><a href="{{ url("iglesias/{$ayuda->iglesia->id}") }}"> {{ $ayuda->iglesia->nombre }} </a></td>
           <td> {{ $ayuda->observaciones }}</td>
           <td>
